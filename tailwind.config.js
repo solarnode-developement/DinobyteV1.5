6831d42326d326c1617f70aefa2f938ddc24ba87
xyzprojectz <m1lkyt.republic@gmail.com>
2025-02-11 22:53:16 +0000

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'lavender-blue': '#E6E6FA',
+        'dark-gray': '#333333',
+        'white': '#FFFFFF',
+        'accent': '#A78BFA',
+        'blue': '#3B82F6',
+      },
+    },
+  },
+  plugins: [],
+}
